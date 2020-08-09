e75964c2bb1c01e17d15b6b8512a517e8aedc51f
Emerson Moniz <emersonmoniz@gmail.com>
2020-08-09 15:03:02 -0400

    Current final Project

---
+++ scatterDot.jl
@@ -0,0 +1,33 @@
+using Base.Threads;
+import Statistics
+
+function computeDotProduct(N)
+    #=
+    Computes dot product of N
+    Returns the sum of Dot Product
+    =#
+    a=fill(2.0,N)
+    b=fill(2.0,N)
+    result = 0
+    for i = 1:N
+        result += a[i] .* b[i]
+    end
+    return result
+end
+
+N=10000000
+nt = nthreads()
+println("The number of threads : $nt")
+println("N is: $N")
+smallDotProducts = zeros(nt)
+timeZ = zeros(nt)
+numS = convert(Int64,N/nt)
+@threads for i=1:nt
+    timestart = time()
+	smallDotProducts[i] = computeDotProduct(numS)
+    timeZ[i] = time() -timestart
+end
+elapsed = Statistics.mean(timeZ)
+totalResult = convert(Int64,sum(smallDotProducts))
+println("The sum for all dot products is: $totalResult")
+println("The elapsed time : $elapsed seconds")
---
+++ scatterDotV.jl
@@ -0,0 +1,44 @@
+using Base.Threads;
+import Statistics
+
+function computeDotProduct(N)
+    #=
+    Computes dot product of N
+    Returns the sum of Dot Product
+    =#
+    a=fill(2.0,N)
+    b=fill(2.0,N)
+    result = 0
+    for i = 1:N
+        result += a[i] .* b[i]
+    end
+    return result
+end
+
+function findRemainder(num,thread)
+    return convert(Int64,num%thread)
+end
+
+N=10000001
+nt = nthreads()
+println("The number of threads : $nt")
+println("N is: $N")
+smallDotProducts = zeros(nt)
+timeZ = zeros(nt)
+numS = N/nt
+numS = floor(numS)
+numS = convert(Int64,numS)
+remainder = findRemainder(N,nt)
+@threads for i=1:nt
+    timestart = time()
+    if i == nt
+        global numS += remainder
+    end
+	smallDotProducts[i] = computeDotProduct(numS)
+    timeZ[i] = time() -timestart
+end
+elapsed = Statistics.mean(timeZ)
+#totalResult= 
+totalResult = convert(Int64,sum(smallDotProducts))
+println("The sum for all dot products is: $totalResult")
+println("The elapsed time : $elapsed seconds")
